2dfd75466f46de588192c910151d5a424ef94393
James Quick <james.q.quick@outlook.com>
2020-10-07 14:09:16 -0500

    logout

--- functions/logout.js
+++ functions/logout.js
@@ -5,10 +5,17 @@ exports.handler = async (event, context) => {
     try {
         const authUtils = new AuthUtils();
         const logoutCookie = authUtils.generateLogoutCookie();
+
+        const auth0DomainLogout = `https://${process.env.AUTH0_DOMAIN}v2/logout`;
+        const urlReturnTo = `returnTo=${process.env.APP_DOMAIN}`;
+        const urlClientId = `client_id=${process.env.AUTH0_CLIENT_ID}`;
+        const logoutUrl = encodeUri(
+            `${auth0DomainLogout}?${urlReturnTo}&${urlClientId}`
+        );
         return {
             statusCode: 302,
             headers: {
-                Location: `https://${process.env.AUTH0_DOMAIN}/v2/logout?returnTo=${process.env.APP_DOMAIN}&client_id=${process.env.AUTH0_CLIENT_ID}`,
+                Location: logoutUrl,
                 'Cache-Control': 'no-cache',
                 'Set-Cookie': logoutCookie,
             },
